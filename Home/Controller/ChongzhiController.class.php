ac93ebe0d4a73cfebf645e35764072f944e20cfa
guilingzhe <1229742581@qq.com>
2017-08-21 22:17:10 +0800

    用户中心

--- Home/Controller/ChongzhiController.class.php
+++ Home/Controller/ChongzhiController.class.php
@@ -3,6 +3,8 @@ namespace Home\Controller;
 use Think\Controller;
 class ChongzhiController extends Controller {
     public function index(){
+    	
         $this->display('user_admin/chongzhi/index');
     }
+
 }
