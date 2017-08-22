8b9484c534b18fe348006b741cb820b16a921300
guilingzhe <1229742581@qq.com>
2017-08-22 09:20:25 +0800

    用户中心修改

--- Home/Controller/IndexUserController.class.php
+++ Home/Controller/IndexUserController.class.php
@@ -5,7 +5,5 @@ class IndexUserController extends Controller {
     public function index(){
         $this->display('user_admin/index/index');
     }
-    public function layout(){
-    	$this->display('user_admin/index/layout');
-    }
+    
 }
